7a3bb84a169344c083ab2de6caacd6326bb843f4
Little-Ker <46097578+Little-Ker@users.noreply.github.com>
2021-11-29 23:39:29 +0800

    [新增] bootstrap

---
+++ src/components/Carousel.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="carousel">
+        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="https://picsum.photos/1000?random=1" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="https://picsum.photos/1000?random=20" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="https://picsum.photos/1000?random=3" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-target="#carouselExampleFade" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-target="#carouselExampleFade" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Carousel',
+  mounted() {
+    $('.carousel').carousel();
+  }
+}
+</script>
