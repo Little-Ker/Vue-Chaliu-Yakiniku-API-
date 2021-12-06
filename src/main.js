23fefc98feddee794588b22b8a2cf1a2431bac53
Little-Ker <46097578+Little-Ker@users.noreply.github.com>
2021-12-06 16:39:49 +0800

    [新增] fontello

--- src/main.js
+++ src/main.js
@@ -6,4 +6,7 @@ import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 
+// fontello
+import '../src/assets/fontello/css/fontello.css';
+
 createApp(App).use(store).use(router).mount('#app')
