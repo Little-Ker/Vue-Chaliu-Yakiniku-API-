2042324b7fb7e25d86706289cab26adb8421d2d4
Little-Ker <46097578+Little-Ker@users.noreply.github.com>
2021-12-26 23:34:59 +0800

    [修改] 調整滾動條樣式

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,20 @@ export default {
   text-align: left;
 }
 
+/* width */
+::-webkit-scrollbar {
+  width: 5px;
+}
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1; 
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
