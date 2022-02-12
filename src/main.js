3037b463caee42a6a34d30b3537a84a5a75427bd
Little-Ker <46097578+Little-Ker@users.noreply.github.com>
2022-02-12 20:01:24 +0800

    [新增] axios 套件

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
@@ -12,4 +14,4 @@ import '../src/assets/fontello/css/fontello.css';
 // scss
 import '@/assets/scss/_all.scss'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(VueAxios, axios).mount('#app')
